dc3d8865f1b5c409446844fd186494e967490128
Faith Moraa <moraaf770@gmail.com>
2022-11-30 12:39:51 +0300

    Gives demerit as integer instead of a float

--- speed-detector.js
+++ speed-detector.js
@@ -5,7 +5,7 @@ let speed = prompt("Speed in km: ")
 //Maximum speed is 70kms
 //demerits should be a maximum of 12 points, anything more than that "License is supended"
 function carSpeed (){
-    let points =((speed - 70)/5)
+    let points = Math.floor(((speed - 70)/5))
      if(speed<=70){
         return `Okay`
         }else if (speed > 70 && points <=12) {
